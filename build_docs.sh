2c036cf6096fd273689e5bfbc464b6f0539111b6
Kamil Kucharski <kamil.kucharski@makimo.pl>
2022-04-11 19:12:57 +0200

    Test

--- build_docs.sh
+++ build_docs.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-REPO="$1"
+REPO="$1" ### Path to the repository.
+WIKI_PAGE="$2" ### Name of the WIKI file.
+DOCS_PATH="$3" ### Path to the C4 files.
 
-### Generate hashes of the files to remove caching issues. 
+### Generate hashes of the files to remove caching issues.
+### Using -hex to not generate the `/` inside a hash.
 CONTEXT_FILE="$(openssl rand -hex 12)_context.svg"
 SYSTEM_FILE="$(openssl rand -hex 12)_system.svg"
 CONTAINER_FILE="$(openssl rand -hex 12)_container.svg"
@@ -14,9 +17,10 @@ rm -rf "$REPO.wiki/diagrams/"
 mkdir "$REPO.wiki/diagrams/"
 
 ### Copy diagrams with hash.
-cp ./generated/context.svg "$REPO.wiki/diagrams/$CONTEXT_FILE"
-cp ./generated/Test/system.svg "$REPO.wiki/diagrams/$SYSTEM_FILE"
-cp ./generated/Test/Backend/container.svg "$REPO.wiki/diagrams/$CONTAINER_FILE"
+cp -r "$DOCS_PATH/" "$REPO.wiki/diagrams/"
+#cp "$DOCS_PATH/context.svg" "$REPO.wiki/diagrams/$CONTEXT_FILE"
+#cp "$DOCS_PATH/Test/system.svg" "$REPO.wiki/diagrams/$SYSTEM_FILE"
+#cp "$DOCS_PATH/Test/Backend/container.svg" "$REPO.wiki/diagrams/$CONTAINER_FILE"
 
 cd $REPO.wiki
 
@@ -25,24 +29,27 @@ git config --global user.email "kaniak274@gmail.com"
 git config --global user.name "Kamil Kucharski"
 
 ### Create wiki page based on the template with injected hashed files.
-cat > Custom.md <<- EOM
-## C1 Diagram
-
-![context](./diagrams/$CONTEXT_FILE)
-
-## C2 Diagram
-
-![context](./diagrams/$SYSTEM_FILE)
-
-## Backend C3 Diagram
-
-![context](./diagrams/$CONTAINER_FILE)
-
+#cat > "$WIKI_PAGE" <<- EOM
+### C1 Diagram
+#
+#![context_diagram](./diagrams/$CONTEXT_FILE)
+#
+### C2 Diagram
+#
+#![system_diagram](./diagrams/$SYSTEM_FILE)
+#
+### Backend C3 Diagram
+#
+#![backend_container_diagram](./diagrams/$CONTAINER_FILE)
+#
+#EOM
+cat > "$WIKI_PAGE" <<- EOM
+[test](./diagrams/Test.md)
 EOM
 
 ### Push changes to the WIKI.
 git add diagrams/
-git add Custom.md
+git add "$WIKI_PAGE"
 
 git diff-index --quiet HEAD || git commit -m "Update diagrams"
 git push
