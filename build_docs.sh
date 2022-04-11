5442218f171f108a41047f4fcf5cacc910b74654
Kamil Kucharski <kamil.kucharski@makimo.pl>
2022-04-11 18:43:04 +0200

    Change build docs script

--- build_docs.sh
+++ build_docs.sh
@@ -2,20 +2,30 @@
 
 set -e
 
-CONTEXT_FILE="$(openssl rand -base64 12)_context.svg"
-SYSTEM_FILE="$(openssl rand -base64 12)_system.svg"
-CONTAINER_FILE="$(openssl rand -base64 12)_container.svg"
 REPO="$1"
 
-mv ./generated/context.svg "$REPO.wiki/$CONTEXT_FILE"
-mv ./generated/Test/system.svg "$REPO.wiki/$SYSTEM_FILE"
-mv ./generated/Test/Backend/container.svg "$REPO.wiki/$CONTAINER_FILE"
+### Generate hashes of the files to remove caching issues. 
+CONTEXT_FILE="$(openssl rand -hex 12)_context.svg"
+SYSTEM_FILE="$(openssl rand -hex 12)_system.svg"
+CONTAINER_FILE="$(openssl rand -hex 12)_container.svg"
+
+### Recreate diagrams directory.
+rm -rf "$REPO.wiki/diagrams/"
+mkdir "$REPO.wiki/diagrams/"
+
+### Copy diagrams with hash.
+cp ./generated/context.svg "$REPO.wiki/diagrams/$CONTEXT_FILE"
+cp ./generated/Test/system.svg "$REPO.wiki/diagrams/$SYSTEM_FILE"
+cp ./generated/Test/Backend/container.svg "$REPO.wiki/diagrams/$CONTAINER_FILE"
 
 cd $REPO.wiki
+
+### Set git configs to push to the WIKI
 git config --global user.email "kaniak274@gmail.com"
 git config --global user.name "Kamil Kucharski"
 
-CUSTOM_FILE = """
+### Create wiki page based on the template with injected hashed files.
+cat > Custom.md <<- EOM
 ## C1 Diagram
 
 ![context]($CONTEXT_FILE)
@@ -28,13 +38,10 @@ CUSTOM_FILE = """
 
 ![context]($CONTAINER_FILE)
 
-"""
+EOM
 
-echo "$CUSTOM_FILE" > Custom.md
-
-git add "$CONTEXT_FILE"
-git add "$SYSTEM_FILE"
-git add "$CONTAINER_FILE"
+### Push changes to the WIKI.
+git add diagrams/
 git add Custom.md
 
 git diff-index --quiet HEAD || git commit -m "Update diagrams"
